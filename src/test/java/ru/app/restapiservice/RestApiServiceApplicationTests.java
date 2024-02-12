2f6925755c3f05959809d6acdbd3e5d55d99b549
Bityta <bituckihs@gmail.com>
2024-02-12 20:36:39 +0300

    Created clean project using spring web + spring Data Jpa + lombok

---
+++ src/test/java/ru/app/restapiservice/RestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.restapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
