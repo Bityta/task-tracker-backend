63ca6b330e029876bd4be2046138954571d96fca
Bityta <bituckihs@gmail.com>
2024-02-12 22:34:08 +0300

    a combination of user and role

--- src/main/java/ru/app/restapiservice/service/UserService.java
+++ src/main/java/ru/app/restapiservice/service/UserService.java
@@ -3,9 +3,15 @@ package ru.app.restapiservice.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.app.restapiservice.model.Role;
+import ru.app.restapiservice.model.RoleEnum;
 import ru.app.restapiservice.model.User;
+import ru.app.restapiservice.repository.RoleRepository;
 import ru.app.restapiservice.repository.UserRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 
@@ -14,13 +20,33 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
 
     //todo
-    //добавление роль и инкодинг пароля
+    //инкодинг пароля
     @Transactional
     public void addUser(User user) {
+
+        Role role = Role.builder()
+                .owner(user)
+                .role(RoleEnum.USER)
+                .build();
+
+        user.setRole(role);
+
+
+        try {
+            Date date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse("2012-05-20T09:00:00.000Z");
+            String formattedDate = new SimpleDateFormat("dd/MM/yyyy, Ka").format(date);
+            user.setDateOfRegistration(formattedDate);
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
         this.userRepository.save(user);
+        this.roleRepository.save(role);
     }
 
     public Optional<User> findByEmail(String email) {
