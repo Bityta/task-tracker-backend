c14801e74695394f3c0d92be4c583068e209062d
Bityta <bituckihs@gmail.com>
2024-02-16 01:31:41 +0300

    adding methods for registration and authorization

--- src/main/java/ru/app/restapiservice/controller/UserController.java
+++ src/main/java/ru/app/restapiservice/controller/UserController.java
@@ -2,9 +2,13 @@ package ru.app.restapiservice.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ru.app.restapiservice.model.User;
+import ru.app.restapiservice.security.service.AuthService;
 import ru.app.restapiservice.service.UserService;
 
 import java.util.List;
@@ -15,17 +19,37 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final AuthService authService;
 
-    @PostMapping()
-    public void test(@RequestBody User user) {
+    @PostMapping("/reg")
+    public ResponseEntity<?> register(@RequestBody User user) {
 
-        this.userService.addUser(user);
+        if (!userService.existByEmail(user.getEmail())) {
+            return ResponseEntity.ok(authService.register(user));
+        }
+
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+
     }
+
+    @PostMapping("/log")
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        if (userService.existByEmail(user.getEmail())) {
+            return ResponseEntity.ok(authService.authenticate(user));
+        }
+
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+
+
+    }
+
 
     @GetMapping("/hello")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
-    public String hello() {
-        return "Hello User";
+    public String hello(@AuthenticationPrincipal User user) {
+        System.out.println(user);
+        return "Hello " + user.getFirstName();
     }
 
 
