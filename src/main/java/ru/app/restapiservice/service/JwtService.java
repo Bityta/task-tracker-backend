0855a70f0ae442a98cc7d1d3d4579d8bb2e03b13
Bityta <bituckihs@gmail.com>
2024-02-15 23:24:22 +0300

    creating a service for working with a jwt token

---
+++ src/main/java/ru/app/restapiservice/service/JwtService.java
@@ -0,0 +1,76 @@
+package ru.app.restapiservice.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.app.restapiservice.model.User;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${secret_key}")
+    private String SECRET_KEY;
+
+    @Value("${life_time}")
+    private int LIFE_TIME;
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token, User user) {
+        String email = extractEmail(token);
+        return email.equals(user.getEmail()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String generateToken(User user) {
+
+        return Jwts
+                .builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + LIFE_TIME))
+                .signWith(getSigningKey())
+                .compact();
+
+    }
+
+    private SecretKey getSigningKey() {
+
+        byte[] keyBites = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBites);
+
+    }
+
+
+}
