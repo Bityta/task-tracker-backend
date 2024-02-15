471bd40750031adffa8c3e076550c8c14942c83a
Bityta <bituckihs@gmail.com>
2024-02-16 01:34:39 +0300

    created a class to store the token

---
+++ src/main/java/ru/app/restapiservice/security/model/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package ru.app.restapiservice.security.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponse {
+
+    private String token;
+
+
+}
