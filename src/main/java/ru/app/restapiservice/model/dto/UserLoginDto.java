f81750cc10729c4c085a698ba7521e78121d1745
Bityta <bituckihs@gmail.com>
2024-02-16 16:25:41 +0300

    created a User Login Dto and configured validation

---
+++ src/main/java/ru/app/restapiservice/model/dto/UserLoginDto.java
@@ -0,0 +1,28 @@
+package ru.app.restapiservice.model.dto;
+
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class UserLoginDto {
+
+
+    @Size(min = 5, max = 50, message = "Email must contain from 5 to 50 characters")
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    private String email;
+
+
+    @Size(min = 5, max = 50, message = "Password must contain from 5 to 50 characters")
+    @NotBlank(message = "Password cannot be empty")
+    @Transient
+    private String password;
+
+
+}
