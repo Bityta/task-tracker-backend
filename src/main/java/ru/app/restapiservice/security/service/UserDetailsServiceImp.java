43a3c21c9e5bc6a1d13cb7cdb9aac974584c44b1
Bityta <bituckihs@gmail.com>
2024-02-17 22:55:35 +0300

    fixing the package name

--- src/main/java/ru/app/restapiservice/security/service/UserDetailsServiceImp.java
+++ src/main/java/ru/app/restapiservice/security/service/UserDetailsServiceImp.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-import ru.app.restapiservice.exeption.customException.UserNotFoundException;
+import ru.app.restapiservice.exception.customException.UserNotFoundException;
 import ru.app.restapiservice.model.User;
 import ru.app.restapiservice.security.model.UserDetailsImp;
 import ru.app.restapiservice.service.UserService;
