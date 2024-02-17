f91c739525c291fdc05c480f616d089e92b6152c
Bityta <bituckihs@gmail.com>
2024-02-17 18:16:35 +0300

    adding a new converter

--- src/main/java/ru/app/restapiservice/model/mapper/TaskMapper.java
+++ src/main/java/ru/app/restapiservice/model/mapper/TaskMapper.java
@@ -2,6 +2,7 @@ package ru.app.restapiservice.model.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.app.restapiservice.model.Task;
+import ru.app.restapiservice.model.dto.task.TaskDto;
 import ru.app.restapiservice.model.dto.task.TaskDtoView;
 
 @Component
@@ -15,6 +16,15 @@ public class TaskMapper {
                 .description(task.getDescription())
                 .isCompleted(task.isCompleted())
                 .dateCompleted(task.getDateCompleted())
+                .build();
+
+    }
+
+    public Task map(TaskDto taskDto) {
+
+        return Task.builder()
+                .header(taskDto.getHeader())
+                .description(taskDto.getDescription())
                 .build();
 
     }
