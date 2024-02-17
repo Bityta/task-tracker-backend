ab2d8c4c70d075a885e3d2ab1fcf781f3338710f
Bityta <bituckihs@gmail.com>
2024-02-17 17:58:22 +0300

    added task dto validation

--- src/main/java/ru/app/restapiservice/model/dto/task/TaskDto.java
+++ src/main/java/ru/app/restapiservice/model/dto/task/TaskDto.java
@@ -1,7 +1,18 @@
 package ru.app.restapiservice.model.dto.task;
 
 
-//todo
-//создание + валидация
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
 public class TaskDto {
+
+    @Size(min = 5, max = 50, message = "Header must contain from 5 to 50 characters")
+    @NotBlank(message = "Header cannot be empty")
+    private String header;
+
+    @Size(min = 5, max = 4096, message = "Description must contain from 5 to 4096 characters")
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
 }
