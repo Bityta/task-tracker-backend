aac844216a82fb22d1b620dd6d9c86e225e8938c
Bityta <bituckihs@gmail.com>
2024-02-19 21:27:09 +0300

    Updating error output

--- src/main/java/ru/app/restapiservice/security/exeptionHandler/AuthenticationExceptionHandler.java
+++ src/main/java/ru/app/restapiservice/security/exeptionHandler/AuthenticationExceptionHandler.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.app.restapiservice.api.model.dto.error.ErrorMessageDtoView;
-import ru.app.restapiservice.exception.EmailIsAlreadyUsedException;
-import ru.app.restapiservice.security.config.AuthenticationController;
+import ru.app.restapiservice.exception.customException.EmailIsAlreadyUsedException;
+import ru.app.restapiservice.exception.model.dto.ErrorMessageDtoView;
+import ru.app.restapiservice.security.controller.AuthenticationController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice(assignableTypes = AuthenticationController.class)
 @Hidden()
@@ -24,7 +25,12 @@ public class AuthenticationExceptionHandler {
     @ExceptionHandler(EmailIsAlreadyUsedException.class)
     public Map<String, String> handleEmailIsAlreadyUsedExceptions(EmailIsAlreadyUsedException ex) {
 
-        ErrorMessageDtoView errors = new ErrorMessageDtoView("Email", ex.getMessage());
+
+        ErrorMessageDtoView errors = ErrorMessageDtoView.builder()
+                .status(HttpStatus.CONFLICT)
+                .error(ex.getMessage())
+                .path("/reg")
+                .build();
 
         return errors.getError();
     }
@@ -33,7 +39,11 @@ public class AuthenticationExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public Map<String, String> handleBadCredentialsExceptions(BadCredentialsException ex) {
 
-        ErrorMessageDtoView errors = new ErrorMessageDtoView("Credentials", ex.getMessage());
+        ErrorMessageDtoView errors = ErrorMessageDtoView.builder()
+                .status(HttpStatus.UNAUTHORIZED)
+                .error(ex.getMessage())
+                .path("/")
+                .build();
 
         return errors.getError();
     }
@@ -52,7 +62,16 @@ public class AuthenticationExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
 
-        return errors;
+
+        ErrorMessageDtoView error = ErrorMessageDtoView.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .error(errors.keySet().stream()
+                        .map(errors::get)
+                        .collect(Collectors.joining(". ")))
+                .path("/")
+                .build();
+
+        return error.getError();
     }
 
 }
