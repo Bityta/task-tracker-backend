43beae2b8486330bb5474367fa1b386170477d65
Bityta <bituckihs@gmail.com>
2024-02-19 21:27:43 +0300

    transfer to another pocket: config -> controller

--- src/main/java/ru/app/restapiservice/security/controller/AuthenticationController.java
+++ src/main/java/ru/app/restapiservice/security/controller/AuthenticationController.java
@@ -1,4 +1,4 @@
-package ru.app.restapiservice.security.config;
+package ru.app.restapiservice.security.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
