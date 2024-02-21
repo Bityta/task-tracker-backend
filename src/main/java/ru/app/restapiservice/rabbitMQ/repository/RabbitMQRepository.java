5442bc6bb188ca8c27eded543a2fc0afe32fc4a5
Bityta <bituckihs@gmail.com>
2024-02-21 20:10:04 +0300

    bunch of microservices

---
+++ src/main/java/ru/app/restapiservice/rabbitMQ/repository/RabbitMQRepository.java
@@ -0,0 +1,16 @@
+package ru.app.restapiservice.rabbitMQ.repository;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("task-tracker-email-sender")
+public interface RabbitMQRepository {
+
+
+    @PostMapping("/message/greetings")
+    ResponseEntity<String> sendGreetingsMessage(@RequestBody String email);
+
+
+}
