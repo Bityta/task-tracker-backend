239e9eb62f4494fff6a6baf3fb012683102d4932
Bityta <bituckihs@gmail.com>
2024-02-21 20:37:54 +0300

    update annotation @Server -> @Service

---
+++ src/main/java/ru/app/restapiservice/rabbitMQ/service/RabbitMQService.java
@@ -0,0 +1,18 @@
+package ru.app.restapiservice.rabbitMQ.service;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.app.restapiservice.rabbitMQ.repository.RabbitMQRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RabbitMQService {
+
+    private final RabbitMQRepository rabbitMQRepository;
+
+
+    public void sendGreetingsMessage(String email) throws FeignException {
+        this.rabbitMQRepository.sendGreetingsMessage(email);
+    }
+}
