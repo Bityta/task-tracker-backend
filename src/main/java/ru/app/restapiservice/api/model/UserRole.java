38d7f925484a5ac9944721418c434dd46d69c7cf
Bityta <bituckihs@gmail.com>
2024-02-25 22:38:14 +0300

    @data -> @Getter and @Setter

--- src/main/java/ru/app/restapiservice/api/model/UserRole.java
+++ src/main/java/ru/app/restapiservice/api/model/UserRole.java
@@ -1,15 +1,13 @@
 package ru.app.restapiservice.api.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
