fefd539ed1e335f8092a9eb757614fa7a2c1c1da
Bityta <bituckihs@gmail.com>
2024-02-25 22:37:43 +0300

    add new query

--- src/main/java/ru/app/restapiservice/api/repository/TaskRepository.java
+++ src/main/java/ru/app/restapiservice/api/repository/TaskRepository.java
@@ -3,6 +3,8 @@ package ru.app.restapiservice.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.app.restapiservice.api.model.Task;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> getByOwner_Email(String email);
 }
